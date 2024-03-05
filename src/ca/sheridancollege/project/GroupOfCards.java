a5bdd35741caa8ae68ce735f1c0c2ef8a6f1268e
Jaskaran Kaur <kau15645@sheridancollege.ca>
2024-03-04 21:19:03 -0500

    GroupOfCards Class Modified

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -1,55 +1,67 @@
 /**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ *GroupOfCards Class
  */
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
- * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
- * once. The group of cards has a maximum size attribute which is flexible for reuse.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Jaskaran Kaur, Komalpreet Kaur, Jasmeen Kaur, Prinka 
+ *          March 2024
  */
 public class GroupOfCards {
 
-    //The group of cards, stored in an ArrayList
-    private ArrayList<Card> cards;
-    private int size;//the size of the grouping
-
-    public GroupOfCards(int size) {
-        this.size = size;
-    }
-
-    /**
-     * A method that will get the group of cards as an ArrayList
-     *
-     * @return the group of cards.
-     */
-    public ArrayList<Card> getCards() {
-        return cards;
-    }
-
+   //List of Cards
+    
+    List<Card> groupofcards = new ArrayList<Card>();
+  
+//Shuffle Method to shuffle the cards (randomized the order of the cards)
+    
     public void shuffle() {
-        Collections.shuffle(cards);
+        Collections.shuffle(groupofcards);
     }
 
-    /**
-     * @return the size of the group of cards
-     */
-    public int getSize() {
-        return size;
+//playCard method to show the playedCard
+    
+    public Card playCard(){
+        Card playedCard = this.groupofcards.get(0);
+        this.groupofcards.remove(0);
+        return playedCard;
     }
-
-    /**
-     * @param size the max size for the group of cards
-     */
-    public void setSize(int size) {
-        this.size = size;
+    
+//loop through all the card values and suits to add the whole groupOfCards
+    public GroupOfCards(){
+        for (int k = 2; k <=14; k++){
+            for (int i = 0; i < 4; i++){
+              groupofcards.add(new Card(i, k));
+            }
+        }
     }
-
-}//end class
+    
+//getters and setters for getting the cards from groupofcards and setting the order after shuffling
+    
+    public List<Card> getCards(){
+        return groupofcards;
+    }
+    
+    public void setCards(List<Card> groupofcards){
+        this.groupofcards = groupofcards;
+    }
+    
+    
+//draw method : removes and returns the top card from the cards
+    
+    public Card draw() {
+        if (groupofcards.isEmpty()){
+            System.out.println("No more cards.");
+        return null;
+        } else{
+            Card drawTopCard = groupofcards.get(0);
+            groupofcards.remove(0);
+            return drawTopCard;
+        }
+        
+    }
+}
