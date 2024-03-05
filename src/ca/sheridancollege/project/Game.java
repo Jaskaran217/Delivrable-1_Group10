d2a2bd06f4ed8cd4a86a7bf1ca7bc4b97b45b350
Jaskaran Kaur <kau15645@sheridancollege.ca>
2024-03-04 21:18:17 -0500

    Game Class Modified

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -1,58 +1,76 @@
 /**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * War Card Game
+ * Creating new group of cards
+ * Shuffle the cards to randomized the order of cards
+ * Set the players
+ * Distributing the cards between players
+ * Play the game
+ * Declare the winner
  */
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 
 /**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Jaskaran Kaur , Komalpreet Kaur, Jasmeen Kaur, Prinka
+ *          March 2024
  */
-public abstract class Game {
+public class Game {
+    
+//main method 
+    public static void main(String[] args){
+       
+       //Instantiate GroupOfCards and two players and called the shuffle method        
 
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
-
-    public Game(String name) {
-        this.name = name;
-        players = new ArrayList();
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+       
+        
+        GroupOfCards groupofcards = new GroupOfCards();
+        groupofcards.shuffle();
+        
+        //loop to iterate 26 cards to each player's hand which equals to 52 cards all together       
+        
+        for (int i =1; i <= 26; i++){
+            player1.hand.add(groupofcards.draw()); //players drawing the cards from groupofcards
+            player2.hand.add(groupofcards.draw());
+           
+        }
+        
+       //cards will be described after they are drawn
+        player1.describe();
+        player2.describe();
+        
+        
+        //loop to iterate 26 times for each card for each player and calls the  flip method
+        for (int i = 0; i < 26; i++){
+            Card player1Card = player1.flip();
+            Card player2Card = player2.flip();
+            
+           
+           //comparing the value of each card returned by the players after flipping 
+          //and calls the increment method on player  (who has higher value)
+          
+            if (player1Card.getCardValue()> player2Card.getCardValue()) {
+                player1.incrementScore();
+            } else if (player1Card.getCardValue() < player2Card.getCardValue()){
+                player2.incrementScore();
+            } else {
+                System.out.println();
+            }
+            
+        } 
+        
+        // comparing the final score of the players
+        if (player1.score > player2.score){
+            System.out.println("Player 1 " +" is the winner!!! With a Score of " + player1.score );
+            System.out.println( "Player 2 loses with the score of " + player2.score);
+        } else if (player2.score > player1.score) {
+            System.out.println("Player 2 "+ " is the winner!!! With a Score of "+ player2.score);
+            System.out.println("Player 1 loses with the score of " + player1.score);
+        } else {
+            System.out.println("It's a tie!!!");
+        }
+    
     }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @return the players of this game
-     */
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
-
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
-    }
-
-    /**
-     * Play the game. This might be one method or many method calls depending on your game.
-     */
-    public abstract void play();
-
-    /**
-     * When the game is over, use this method to declare and display a winning player.
-     */
-    public abstract void declareWinner();
-
-}//end class
+}
